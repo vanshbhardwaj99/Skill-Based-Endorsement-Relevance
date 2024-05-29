7d67ed4fd274a994a3237ed592995b47189d0158
Vansham Bhardwaj <vanshbhardwaj99@gmail.com>
2024-05-29 22:04:37 +0530

    Base Commit

---
+++ src/test/java/com/skill/endorsement/EndorsementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skill.endorsement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndorsementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
